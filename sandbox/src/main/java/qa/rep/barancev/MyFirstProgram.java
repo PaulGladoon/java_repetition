51baabc61f836327aa7eafe0e7adc29d53f39186
Paul <paul.gladoon@gmail.com>
2018-01-23 22:38:41 +0200

    add Homework 2

--- sandbox/src/main/java/qa/rep/barancev/MyFirstProgram.java
+++ sandbox/src/main/java/qa/rep/barancev/MyFirstProgram.java
@@ -4,7 +4,11 @@ public class MyFirstProgram {
 
     public static void main (String[] args) {
 
-        System.out.println("Hello World");
+        Point p1 = new Point(10, 5);
+        Point p2 = new Point(15, 8);
+
+        System.out.println(p2.distance(p1));
+
 
     }
 
